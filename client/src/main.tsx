0d8f4eb848efb1feaca646286921789f18ba0f6e
Oriana Schinocca <orianaschinocca@gmail.com>
2026-01-06 20:45:11 -0300

    fix(main): check root element before calling createRoot

---
+++ client/src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.tsx'
+import './index.css'
+
+// Obtener el elemento root y comprobar existencia en tiempo de ejecución
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Root element not found: ensure client/index.html contains <div id="root"></div>');
+}
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
